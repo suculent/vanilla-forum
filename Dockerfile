30c6e269e6957a39a07a48fc787ab62fe78019d8
production <root@thinx.cloud>
2018-05-11 14:13:32 +0000

    added one more theme

--- Dockerfile
+++ Dockerfile
@@ -30,6 +30,8 @@ RUN docker-php-ext-install pdo pdo_mysql \
     && cd themes \
     && wget https://us.v-cdn.net/5018160/uploads/addons/1BBGR0AIXD80.zip \
     && unzip 1BBGR0AIXD80.zip \
+    && wget https://us.v-cdn.net/5018160/uploads/addons/R9QGICV64L1X.zip \
+    && unzip R9QGICV64L1X.zip \
     && echo 'display_errors = Off\nlog_errors = On' >> /usr/local/etc/php/php.ini
 
 VOLUME /var/www/html/uploads
--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 
 # docker run -d -p 8080:80 --name vanillary --rm -v "uploads:/var/www/html/uploads" -v "conf:/var/www/html/conf" severeoverfl0w/vanilla
 
-docker run -d -p 8080:80 --name vanillary -v "uploads:/var/www/html/uploads" -v "conf:/var/www/html/conf" suculent/vanilla-forums
+docker run -d -p 8080:80 --name vanilla-forums --rm -v "uploads:/var/www/html/uploads" -v "conf:/var/www/html/conf" suculent/vanilla-forums:latest
 
